c2ed63892bba3610d381279726cab411e5c6e331
Mariah Sunil Alexiose <mariah.sunilalexiose@gmail.com>
2025-03-11 10:15:14 -0400

    Added tailwind, global variables and styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,8 @@ import { createRoot } from "react-dom/client"
 
 import App from "./App.jsx"
 
+import "./globals.css"
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
