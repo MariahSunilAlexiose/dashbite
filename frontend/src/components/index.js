05c48edb71d3bab14f1ed20ea579092079998922
Mariah Sunil Alexiose <mariah.sunilalexiose@gmail.com>
2025-03-12 18:47:47 -0400

    Moved Header to sections

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,7 +1,6 @@
 import Avatar from "./Avatar"
 import Button from "./Button"
 import Card from "./Card"
-import Header from "./Header"
 import Navbar from "./Navbar"
 
-export { Card, Avatar, Header, Navbar, Button }
+export { Card, Avatar, Navbar, Button }
--- frontend/src/sections/Header.jsx
+++ frontend/src/sections/Header.jsx
@@ -4,12 +4,12 @@ import { Button } from "@cmp"
 
 const Header = () => {
   return (
-    <div className="mx-0 content-center rounded-3xl bg-[url(/src/assets/images/header.jpg)] bg-cover bg-no-repeat xl:h-[45vw] dark:bg-[url(/src/assets/images/dark_mode/header.jpg)]">
+    <div className="mx-0 h-[160vw] content-center rounded-3xl bg-[url(/src/assets/images/header.jpg)] bg-cover bg-no-repeat sm:h-[60vw] xl:h-[45vw] dark:bg-[url(/src/assets/images/dark_mode/header.jpg)]">
       <div className="flex max-w-[55%] flex-col gap-3 p-6 lg:gap-6 lg:pl-20 xl:gap-9">
         <h1 className="lg:leading-18 xl:leading-20">
           Order your <br /> favorite food here
         </h1>
-        <p className="mt-0">
+        <p className="mt-0 text-xs sm:text-sm md:text-base">
           Choose from a diverse menu featuring a delectable array of dishes
           created with the finest ingredients and culinary expertise. Our
           mission is to satisfy your cravings and elevate your dining
