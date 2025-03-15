5d36e66210214b7bc628312097d4cb783ffad9cb
Mariah Sunil Alexiose <mariah.sunilalexiose@gmail.com>
2025-03-15 14:21:34 -0400

    Added admin path aliases

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,3 +1,5 @@
+import path from "path"
+
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
@@ -5,4 +7,17 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@icons": path.resolve(__dirname, "./src/assets/icons"),
+      "@img": path.resolve(__dirname, "./src/assets/images"),
+      "@cmp": path.resolve(__dirname, "./src/components"),
+      "@context": path.resolve(__dirname, "./src/context"),
+      "@providers": path.resolve(__dirname, "./src/providers"),
+      "@sections": path.resolve(__dirname, "./src/sections"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+    },
+  },
 })
