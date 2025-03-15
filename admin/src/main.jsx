de142335988d6e8f704a0bc27dc4411e332b1174
Mariah Sunil Alexiose <mariah.sunilalexiose@gmail.com>
2025-03-15 15:13:24 -0400

    Added ThemeProvider to admin main file

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,4 +1,4 @@
-import React, { StrictMode } from "react"
+import React from "react"
 
 import { createRoot } from "react-dom/client"
 
@@ -6,8 +6,10 @@ import App from "./App.jsx"
 
 import "./styles/globals.css"
 
+import { ThemeProvider } from "@providers"
+
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <ThemeProvider>
     <App />
-  </StrictMode>
+  </ThemeProvider>
 )
