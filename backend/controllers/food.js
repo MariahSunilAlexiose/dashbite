a5ab18d951ebc878e177f7eb9243484b34cc0498
Mariah Sunil Alexiose <mariah.sunilalexiose@gmail.com>
2025-03-15 10:20:35 -0400

    Added add dish controller

---
+++ backend/controllers/food.js
@@ -0,0 +1,23 @@
+import dishModel from "../models/food.js"
+
+// add dish item
+const addDish = async (req, res) => {
+  let image_filename = `${req.file.filename}`
+  const food = new dishModel({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    category: req.body.category,
+    rating: req.body.rating,
+    image: image_filename,
+  })
+  try {
+    await food.save()
+    res.json({ success: true, message: "Dish Added" })
+  } catch (error) {
+    console.log(error)
+    res.json({ success: false, message: "Error" })
+  }
+}
+
+export { addDish }
