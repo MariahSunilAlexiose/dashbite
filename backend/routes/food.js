b6abd6617db703b3518ff0f2969859b4806321c6
Mariah Sunil Alexiose <mariah.sunilalexiose@gmail.com>
2025-03-15 10:21:23 -0400

    Added route to add dish

---
+++ backend/routes/food.js
@@ -0,0 +1,20 @@
+import express from "express"
+import multer from "multer"
+
+import { addDish } from "../controllers/food.js"
+
+const dishRouter = express.Router()
+
+// Image Storage Engine
+const storage = multer.diskStorage({
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    return cb(null, `${Date.now()}${file.originalname}`)
+  },
+})
+
+const upload = multer({ storage: storage })
+
+dishRouter.post("/add", upload.single("image"), addDish)
+
+export default dishRouter
