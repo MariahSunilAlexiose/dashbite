60b1f4b5f4426e44e3ffd3b68db7cdabb9510e76
Mariah Sunil Alexiose <mariah.sunilalexiose@gmail.com>
2025-03-15 14:14:32 -0400

    Added tailwind, global variables, styles, fonts to admin

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -4,6 +4,8 @@ import { createRoot } from "react-dom/client"
 
 import App from "./App.jsx"
 
+import "./styles/globals.css"
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
