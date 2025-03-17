f3ef796c31e970712c2603c04b64d70a7d69c8f5
Mariah Sunil Alexiose <mariah.sunilalexiose@gmail.com>
2025-03-16 21:17:53 -0400

    Updated tsx files to jsx

---
+++ frontend/src/components/Input.jsx
@@ -0,0 +1,66 @@
+import React from "react"
+
+import { Button } from "@cmp"
+import PropTypes from "prop-types"
+
+const Input = ({
+  type,
+  value,
+  name,
+  onChange,
+  id,
+  step,
+  min,
+  max,
+  pattern,
+  placeholder = "",
+  className,
+  icon,
+  iconName,
+}) => {
+  return (
+    <div className="relative flex w-full items-center">
+      {icon && (
+        <img src={icon} alt={iconName} className="absolute left-3 h-5 w-5" />
+      )}
+      <input
+        name={name}
+        type={type}
+        id={id}
+        value={value}
+        step={step}
+        min={min}
+        max={max}
+        onChange={onChange}
+        placeholder={placeholder}
+        pattern={pattern}
+        className={`${className} border-input placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50`}
+        required
+      />
+      {icon && (
+        <Button variant="ghost" className="absolute right-0.5">
+          Apply
+        </Button>
+      )}
+    </div>
+  )
+}
+
+Input.propTypes = {
+  name: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(["text", "number", "email", "tel", "password", "date"])
+    .isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  onChange: PropTypes.func.isRequired,
+  id: PropTypes.string,
+  placeholder: PropTypes.string,
+  step: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  min: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  max: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  pattern: PropTypes.string,
+  className: PropTypes.string,
+  icon: PropTypes.string,
+  iconName: PropTypes.string,
+}
+
+export default Input
