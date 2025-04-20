499a16d0d601b8aaae79e004d322aca7bb6e3a83
Mariah Sunil Alexiose <mariah.sunilalexiose@gmail.com>
2025-04-20 19:55:02 -0400

    Updated OrderTable to show no orders as well

--- frontend/src/components/OrderTable.jsx
+++ frontend/src/components/OrderTable.jsx
@@ -10,46 +10,48 @@ import { TableCell, TableHead, TableRow } from "./Table"
 const OrderTable = ({ orders }) => {
   const { url } = useContext(StoreContext)
   return (
-    <div className="relative w-full overflow-x-auto p-4">
-      {orders ? (
-        <table className="w-full table-auto border-collapse text-sm">
-          <thead>
-            <TableRow className="border-b">
-              <TableHead>OrderID</TableHead>
-              <TableHead>Items</TableHead>
-              <TableHead>Date</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Amount</TableHead>
-            </TableRow>
-          </thead>
-          <tbody>
-            {orders.map((order) => (
-              <TableRow key={order._id} className="border-b">
-                <TableCell>{order._id}</TableCell>
-                <TableCell>
-                  <div className="flex items-center justify-center gap-2">
-                    {order.items &&
-                      order.items.map((item, key) => (
-                        <div key={key} className="relative h-8 w-8">
-                          <img
-                            src={url + "/images/" + item.image}
-                            alt={item.name}
-                            className="h-full w-full rounded"
-                          />
-                          <div className="bg-foreground text-background absolute -right-2 -top-2 rounded-full px-2.5 py-1 text-xs font-bold shadow-md">
-                            {item.quantity}
-                          </div>
-                        </div>
-                      ))}
-                  </div>
-                </TableCell>
-                <TableCell>{formatDate(order.date)}</TableCell>
-                <TableCell>{order.status}</TableCell>
-                <TableCell>${order.amount}</TableCell>
+    <div className="pt-5">
+      {orders && orders.length > 0 ? (
+        <div className="relative w-full overflow-x-auto p-4">
+          <table className="w-full table-auto border-collapse text-sm">
+            <thead>
+              <TableRow className="border-b">
+                <TableHead>OrderID</TableHead>
+                <TableHead>Items</TableHead>
+                <TableHead>Date</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead>Amount</TableHead>
               </TableRow>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {orders.map((order) => (
+                <TableRow key={order._id} className="border-b">
+                  <TableCell>{order._id}</TableCell>
+                  <TableCell>
+                    <div className="flex items-center justify-center gap-2">
+                      {order.items &&
+                        order.items.map((item, key) => (
+                          <div key={key} className="relative h-8 w-8">
+                            <img
+                              src={url + "/images/" + item.image}
+                              alt={item.name}
+                              className="h-full w-full rounded"
+                            />
+                            <div className="bg-foreground text-background absolute -right-2 -top-2 rounded-full px-2.5 py-1 text-xs font-bold shadow-md">
+                              {item.quantity}
+                            </div>
+                          </div>
+                        ))}
+                    </div>
+                  </TableCell>
+                  <TableCell>{formatDate(order.date)}</TableCell>
+                  <TableCell>{order.status}</TableCell>
+                  <TableCell>${order.amount}</TableCell>
+                </TableRow>
+              ))}
+            </tbody>
+          </table>
+        </div>
       ) : (
         <p>You have no orders yet</p>
       )}
