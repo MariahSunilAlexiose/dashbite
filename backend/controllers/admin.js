c4f6112f528659b19f98ac684e205a99e7559405
Mariah Sunil Alexiose <mariah.sunilalexiose@gmail.com>
2025-04-23 17:46:11 -0400

    Made orders in descending order

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -3,7 +3,7 @@ import userModel from "../models/user.js"
 
 const getOrders = async (req, res) => {
   try {
-    const orders = await orderModel.find({}) // Fetch all orders
+    const orders = await orderModel.find({}).sort({ createdAt: -1 }) // Fetch all orders
 
     const transformedOrders = await Promise.all(
       orders.map(async (order) => {
@@ -41,4 +41,47 @@ const getOrders = async (req, res) => {
   }
 }
 
-export { getOrders }
+const getOrderByID = async (req, res) => {
+  try {
+    const { orderID } = req.params
+    const order = await orderModel.findById(orderID)
+
+    if (!order) {
+      res.json({ success: false, message: "Order not found!" })
+      return
+    }
+
+    try {
+      const user = await userModel.findById(order.userID)
+
+      if (user) {
+        // eslint-disable-next-line no-unused-vars
+        const { userID, ...rest } = order._doc
+        res.json({
+          success: true,
+          data: {
+            ...rest,
+            user: user,
+          },
+        })
+      } else {
+        console.error(`User not found for userID: ${order.userID}`)
+        res.json({
+          success: false,
+          message: "User not found for the given order.",
+        })
+      }
+    } catch (error) {
+      console.error(
+        `Error fetching user data for userID ${order.userID}:`,
+        error
+      )
+      res.json({ success: false, message: "Error fetching user data!" })
+    }
+  } catch (error) {
+    console.error(`Error fetching order with ID ${req.params.id}:`, error)
+    res.json({ success: false, message: "Error fetching order!" })
+  }
+}
+
+export { getOrderByID, getOrders }
