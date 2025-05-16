42ffcddd4b4a16854132cf57657d2c5d4126f3dc
Mariah Sunil Alexiose <mariah.sunilalexiose@gmail.com>
2025-05-16 14:57:31 -0400

    Updated MultiSelectDropDown to remove options from input box

--- admin/src/components/MultiSelectDropdown.jsx
+++ admin/src/components/MultiSelectDropdown.jsx
@@ -1,75 +1,136 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 
 import { ChevronUpDownIcon } from "@icons"
 import PropTypes from "prop-types"
 
 const MultiSelectDropDown = ({ options, onChange, defaultValue = [] }) => {
+  const dropdownRef = useRef(null)
   const [open, setOpen] = useState(false)
-  const [selectedValues, setSelectedValues] = useState(defaultValue)
+  const [selectedOptions, setSelectedOptions] = useState([])
 
   useEffect(() => {
-    if (defaultValue.length && options?.length) {
-      const selectedOptions = options.filter((option) =>
+    if (defaultValue.length && options.length) {
+      const updatedSelectedOptions = options.filter((option) =>
         defaultValue.includes(option._id)
       )
-      setSelectedValues(selectedOptions.map((opt) => opt.name))
-      onChange(defaultValue)
+
+      setSelectedOptions((prev) => {
+        const prevIds = prev
+          .map((opt) => opt._id)
+          .sort()
+          .join(",")
+        const newIds = updatedSelectedOptions
+          .map((opt) => opt._id)
+          .sort()
+          .join(",")
+
+        return prevIds !== newIds ? updatedSelectedOptions : prev
+      })
     }
   }, [defaultValue, options])
 
-  const handleSelect = (option) => {
-    const newSelected = selectedValues.includes(option.name)
-      ? selectedValues.filter((val) => val !== option.name)
-      : [...selectedValues, option.name]
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setOpen(false)
+      }
+    }
 
-    setSelectedValues(newSelected)
-    onChange(
-      options
-        .filter((opt) => newSelected.includes(opt.name))
+    document.addEventListener("mousedown", handleClickOutside)
+    return () => document.removeEventListener("mousedown", handleClickOutside)
+  }, [])
+
+  useEffect(() => {
+    onChange(selectedOptions.map((opt) => opt._id))
+  }, [selectedOptions])
+
+  const handleSelect = (option) => {
+    setSelectedOptions((prevSelected) => {
+      const isSelected = prevSelected.some((opt) => opt._id === option._id)
+      const updatedSelected = isSelected
+        ? prevSelected.filter((opt) => opt._id !== option._id)
+        : [...prevSelected, option]
+
+      const prevIds = prevSelected
         .map((opt) => opt._id)
-    )
+        .sort()
+        .join(",")
+      const newIds = updatedSelected
+        .map((opt) => opt._id)
+        .sort()
+        .join(",")
+
+      return prevIds !== newIds ? updatedSelected : prevSelected
+    })
   }
 
   return (
-    <div className="bg-popover text-popover-foreground rounded-md border p-0 shadow-md outline-none">
-      <div className="bg-popover text-popover-foreground flex h-full w-full flex-col overflow-hidden rounded-md">
+    <div
+      ref={dropdownRef}
+      className="rounded-md border p-0 shadow-md outline-none"
+    >
+      <div className="flex h-full w-full flex-col overflow-hidden rounded-md">
         <div className="flex items-center border-b px-3">
           <div
             onClick={() => setOpen(!open)}
-            className="placeholder:text-muted-foreground flex h-10 w-full cursor-pointer items-center rounded-md bg-transparent py-3 text-sm outline-none disabled:cursor-not-allowed disabled:opacity-50"
+            className="flex w-full items-center gap-2 border-b py-2"
           >
-            {selectedValues.length > 0
-              ? selectedValues.join(", ")
-              : "Select options..."}
-            <img
-              src={ChevronUpDownIcon}
-              alt="Chevron Up Down Icon"
-              width={20}
-              height={20}
-              className="ml-auto"
-            />
+            <div className="flex flex-grow flex-wrap items-center gap-2">
+              {selectedOptions.length > 0 ? (
+                selectedOptions.map((option, index) => (
+                  <div
+                    key={option._id || `${option.name}-${index}`}
+                    className="bg-blue-40 flex items-center rounded-md px-3 py-1 text-sm text-white"
+                  >
+                    {option.name}
+                    <button
+                      className="ml-2 text-white hover:text-gray-200"
+                      onClick={() => handleSelect(option)}
+                    >
+                      ✕
+                    </button>
+                  </div>
+                ))
+              ) : (
+                <p className="flex-grow text-sm text-gray-400">
+                  Search an option...
+                </p>
+              )}
+            </div>
+            <div className="ml-auto">
+              <img
+                src={ChevronUpDownIcon}
+                alt="Chevron Up Down Icon"
+                width={20}
+                height={20}
+              />
+            </div>
           </div>
         </div>
         {open && (
           <div className="max-h-[300px] overflow-y-auto overflow-x-hidden">
             <div className="overflow-hidden p-1">
-              {options.map((option, index) => (
-                <div
-                  key={index}
-                  className={`hover:bg-accent/40 relative flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none disabled:pointer-events-none disabled:opacity-50 ${
-                    selectedValues.includes(option.name) ? "bg-accent" : ""
-                  }`}
-                  onClick={() => handleSelect(option)}
-                >
-                  <input
-                    type="checkbox"
-                    checked={selectedValues.includes(option.name)}
-                    className="mr-2"
-                    readOnly
-                  />
-                  {option.name}
+              {options && options.length ? (
+                options.map((option) => (
+                  <div
+                    key={option._id}
+                    className={`hover:bg-border relative flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none ${
+                      selectedOptions.some(
+                        (selected) => selected._id === option._id
+                      )
+                        ? "bg-blue-40 text-white"
+                        : ""
+                    }`}
+                    onClick={() => handleSelect(option)}
+                  >
+                    {option.name}
+                  </div>
+                ))
+              ) : (
+                <div className="py-6 text-center text-sm">
+                  No options found.
                 </div>
-              ))}
+              )}
             </div>
           </div>
         )}
